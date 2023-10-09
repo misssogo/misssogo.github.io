8af0401ef1d0cf29948c645c4c6e8ebd5e36c644
misssogo <lee520@hotmail.com>
2023-10-09 23:40:24 +0800

    Initial commit

--- serviceworker.js
+++ serviceworker.js
@@ -1,39 +1,7 @@
-var CACHE_NAME = 'russian_assets';
+var CACHE_NAME = 'qqbone_assets';
 var CACHE_URLS = [
-    '/dear.html',
-    '/offline.html',
-    '/a.mp3',
-    '/b.mp3',
-    '/v.mp3',
-    '/g.mp3',
-    '/d.mp3',
-    '/ye.mp3',
-    '/yo.mp3',
-    '/zh.mp3',
-    '/z.mp3',
-    '/i.mp3',
-    '/y.mp3',
-    '/k.mp3',
-    '/l.mp3',
-    '/m.mp3',
-    '/n.mp3',
-    '/o.mp3',
-    '/p.mp3',
-    '/r.mp3',
-    '/s.mp3',
-    '/t.mp3',
-    '/oo.mp3',
-    '/f.mp3',
-    '/kh.mp3',
-    '/ts.mp3',
-    '/ch.mp3',
-    '/sh.mp3',
-    '/sshch.mp3',
-    '/no_sound.mp3',
-    '/iy.mp3',
-    '/e.mp3',
-    '/you.mp3',
-    '/ya.mp3'
+    '/index.html',
+    '/offline.html'
 ];
 
 self.addEventListener('install', event => {
